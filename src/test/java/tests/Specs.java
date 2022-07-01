1887f00a8607e3a2402cae3259b8aa00ad64fd2d
Tatiana Kazakova <test.kazakova@gmail.com>
2022-07-01 22:38:52 +0300

    add build, gitignore, TestBase, Interfaces

---
+++ src/test/java/tests/Specs.java
@@ -0,0 +1,38 @@
+package tests;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static helpers.CustomApiListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.ALL;
+
+public class Specs {
+    public static RequestSpecification request;
+
+    static {
+        request = with()
+                .filter(withCustomTemplates())
+                .basePath("/rest")
+                .log().all()
+                .contentType(ContentType.JSON);
+    }
+
+
+    public static ResponseSpecification response200 = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .log(ALL)
+            .build();
+
+    public static ResponseSpecification response403 = new ResponseSpecBuilder()
+            .expectStatusCode(403)
+            .log(ALL)
+            .build();
+
+    public static ResponseSpecification response405 = new ResponseSpecBuilder()
+            .expectStatusCode(405)
+            .log(ALL)
+            .build();
+}
