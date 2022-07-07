2ad16ba3765a53a364cb08392973b42fd7067208
Tatiana Kazakova <test.kazakova@gmail.com>
2022-07-07 12:00:29 +0300

    add build, gitignore, TestBase, Interfaces

--- src/test/java/models/ResponseAuth.java
+++ src/test/java/models/ResponseAuth.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @Data
@@ -19,106 +18,114 @@ public class ResponseAuth {
     private String message;
     private String status;
 
-public class DeviceType {
+    public class Data {
 
-    private String code;
-    private String name;
-    private Integer id;
+        private DeviceType deviceType;
+        private String note;
+        private String address;
+        private Role role;
+        private Boolean showInCalendar;
+        private Org org;
+        private Boolean active;
+        private List<Phones> phones;
+        private Double lon;
+        private Setting setting;
+        private Boolean removed;
+        private String name;
+        private Integer id;
+        private String email;
+        private Double lat;
 
+        public class DeviceType {
+
+            private String code;
+            private String name;
+            private Integer id;
+
+        }
+
+        public class Phones {
+
+            private String note;
+            private Integer phoneNumber;
+            private Boolean mobile;
+            private Boolean main;
+            private Integer id;
+            private String token;
+
+        }
+
+
+        public class Setting {
+
+            private Boolean displayPush;
+            private Integer id;
+
+        }
+
+        public class Role {
+
+            private String code;
+            private String name;
+            private Integer id;
+
+        }
+        public class Org {
+
+            private String note;
+            private Boolean removed;
+            private Integer userCount;
+            private String name;
+            private Integer endLicenseDate;
+            private Integer id;
+            private Setting setting;
+
+            public class Setting {
+
+                private Integer ringCentralSyncSmsTime;
+                private Integer ringCentralSyncCallTime;
+                private Boolean displayMerchantButton;
+                private Boolean displayPromoterButton;
+                private Integer ringCentralExtension;
+                private String taskConfirmationLink;
+                private String serviceRatingText;
+                private Integer phoneCode;
+                private Integer id;
+                private Integer firstHour;
+                private String taskConfirmationText;
+                private String serviceRatingLink;
+                private String ringCentralPassword;
+                private Integer clientNumber;
+                private Integer ringCentralSenderNum;
+                private Integer ringCentralAccountId;
+                private String ringCentralClientId;
+                private Boolean callRecording;
+                private Boolean saveToPhoneBook;
+                private String ringCentralSyncCallToken;
+                private String ringCentralClientSecret;
+                private Integer ringCentralLogin;
+                private Integer timeFormat;
+                private String ringCentralSyncSmsToken;
+                private String orderTimeFormat;
+
+            }
+
+        }
+    }
 }
 
 
-public class Role {
-
-    private String code;
-    private String name;
-    private Integer id;
-
-}
 
 
-public class Setting {
-
-    private Integer ringCentralSyncSmsTime;
-    private Integer ringCentralSyncCallTime;
-    private Boolean displayMerchantButton;
-    private Boolean displayPromoterButton;
-    private Integer ringCentralExtension;
-    private String taskConfirmationLink;
-    private String serviceRatingText;
-    private Integer phoneCode;
-    private Integer id;
-    private Integer firstHour;
-    private String taskConfirmationText;
-    private String serviceRatingLink;
-    private String ringCentralPassword;
-    private Integer clientNumber;
-    private Integer ringCentralSenderNum;
-    private Integer ringCentralAccountId;
-    private String ringCentralClientId;
-    private Boolean callRecording;
-    private Boolean saveToPhoneBook;
-    private String ringCentralSyncCallToken;
-    private String ringCentralClientSecret;
-    private Integer ringCentralLogin;
-    private Integer timeFormat;
-    private String ringCentralSyncSmsToken;
-    private String orderTimeFormat;
-
-}
 
 
-public class Org {
-
-    private String note;
-    private Boolean removed;
-    private Integer userCount;
-    private String name;
-    private Integer endLicenseDate;
-    private Integer id;
-    private Setting setting;
-
-}
 
 
-public class Phones {
 
-    private String note;
-    private Integer phoneNumber;
-    private Boolean mobile;
-    private Boolean main;
-    private Integer id;
-    private String token;
 
-}
 
-/*
-public class Setting {
 
-    private Boolean displayPush;
-    private Integer id;
 
-}
-*/
 
-public class Data {
 
-    private DeviceType deviceType;
-    private String note;
-    private String address;
-    private Role role;
-    private Boolean showInCalendar;
-    private Org org;
-    private Boolean active;
-    private List<Phones> phones;
-    private Double lon;
-    private Setting setting;
-    private Boolean removed;
-    private String name;
-    private Integer id;
-    private String email;
-    private Double lat;
 
-}
-
-}
