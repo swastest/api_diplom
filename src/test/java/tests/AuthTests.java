3f0111049b250336b329df1c7b9fd0a482d4b21b
Tatiana Kazakova <test.kazakova@gmail.com>
2022-07-10 11:34:58 +0300

    1

--- src/test/java/tests/AuthTests.java
+++ src/test/java/tests/AuthTests.java
@@ -13,7 +13,7 @@ import static tests.specification.Specs.response200;
 @Tag("auth")
 public class AuthTests {
     @ParameterizedTest(name = "{2} авторизируется по логину и паролю")
-    @MethodSource(value = "helpers.Params#LoginPasswordParamsAdminManagerTech")
+    @MethodSource(value = "helpers.Params#loginPasswordParamsAdminManagerTech")
     void authTests(String password, String email, String forNameTest) {
         ResponseAuthLombok resp = given()
                 .spec(request)
